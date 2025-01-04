e987e55d2367bb7d551e11d3a3166bca9b00a886
ESSAFI01 <essafianas68@gmail.com>
2025-01-04 23:01:32 +0100

    bghit nbdl branch

--- __manifest__.py
+++ __manifest__.py
@@ -1,19 +1,21 @@
 {
     'name': 'CRM Automation',
     'version': '1.0',
-    'depends': ['base'],
+    'depends': ['base','crm'],
     'category': 'Sales/CRM',
     'summary': 'Automated CRM features with reminders and feedback',
     'description': """
-        This module extends Odoo CRM with:
-        - Auto-archiving of opportunities after 30 days
-        - Automated reminders (7/15/30 days)
-        - Customer feedback surveys
+        This module enhances the Odoo CRM with:
+        - Automatic archiving of opportunities 30 days after their creation.
+        - Automated email reminders for leads and salespeople at 7, 15, and 30 days.
+        - Customer feedback collection through surveys after key activities.
     """,
     'author': 'Bardin lktaf',
     'data': [
-        'security/ir.model.access.csv',
-        'views/my_model_views.xml',
+    'security/ir.model.access.csv', 
+    'views/my_model_views.xml',     
+    'data/cron_jobs.xml',            
+    'data/email_templates.xml',      
     ],
     'installable': True,
     'auto_install': False,
